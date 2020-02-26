762cea80c4bd8d2e804e5d3e78ec44e1b4bfe79e
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-02-26 17:25:24 -0600

    Clase Ingrediente Terminada

---
+++ modules/Ingrediente.js
@@ -0,0 +1,16 @@
+import Cantidad from "./Cantidad.js";
+
+export default class Ingrediente {
+    /**
+     * 
+     * @param {Cantidad} cantidad 
+     * @param {String} nombre 
+     * @param {Number} costo 
+     */
+    constructor(cantidad, nombre, costo) {
+        this.cantidad = cantidad;
+        this.nombre = nombre;
+        this.costo = costo;
+    }
+    getDescripcion = _ => `${this.cantidad.getDescripcion()} de ${this.nombre} $${this.costo}`;
+}
