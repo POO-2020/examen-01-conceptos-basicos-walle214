e2b556f0bd967773290716991c3496ad3e50de0d
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-02-26 17:35:59 -0600

    Clase Receta Terminada

---
+++ modules/Receta.js
@@ -0,0 +1,22 @@
+import Ingrediente from "./Ingrediente.js";
+
+export default class Receta {
+    /**
+     * 
+     * @param {String} nombre 
+     * @param {String} autor 
+     * @param {Ingrediente[]} ingredientes 
+     */
+    constructor(nombre, autor, ingredientes) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.ingredientes = ingredientes
+    }
+    getNumeroIngredientes = _ => this.ingredientes.length;
+    getCosto = _ => this.ingredientes.reduce((previusValue, currentValue, currentIndex) => previusValue + currentValue.costo);
+    imprimirEnConsola = _ => {
+        console.log(this.nombre, 'Autor:' + this.autor);
+        this.ingredientes.forEach(ingrediente => console.log(ingrediente.getDescripcion()));
+    }
+    agregarIngrdiente = ingrediente => this.ingredientes.push(ingrediente);
+}
